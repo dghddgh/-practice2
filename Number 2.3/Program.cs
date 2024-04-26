d5a9fb2ba512a3b1b73b2ab691a50bf36472cef5
dghddgh <152140668+dghddgh@users.noreply.github.com>
2024-04-26 13:28:31 +0700

    commit.cs

--- Number 2.3/Program.cs
+++ Number 2.3/Program.cs
@@ -1,45 +1,40 @@
-using System;
-class Worker
-{
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public int Rate { get; set; }
-    public int days { get; set; }
-    public void GetSalary(int Rate, int days)
+using System; 
+ 
+public class Numbers 
+{ 
+    private int number1 { get; set; } 
+    private int number2 { get; set; }
+ 
+    public Numbers(int num1, int num2) 
+    { 
+        number1 = num1; 
+        number2 = num2; 
+    } 
+ 
+    public void DisplayNumbers() 
+    { 
+        Console.WriteLine($"Число 1: {number1}"); 
+        Console.WriteLine($"Число 2: {number2}"); 
+    } 
+ 
+    public int Sum() 
+    { 
+        return number1 + number2; 
+    } 
+ 
+    public int Max() 
+    { 
+        return Math.Max(number1, number2); 
+    } 
+} 
+ 
+public class Program 
+{ 
+    public static void Main(string[] args) 
     {
-        return Rate * days;
-    }
-}
-class Program
-{
-    static void Main()
-    {
-        
-        Worker worker1 = new Worker();
-        worker1.Name = "Моника";
-        worker1.Surname = "Родригес";
-        worker1.Rate = 20080;
-        worker1.days = 22;
-        
-        Worker worker2 = new Worker();
-        worker2.Name = "Элайджа";
-        worker2.Surname = "Фицджеральд";
-        worker2.Rate = 19035;
-        worker2.days = 18;
-        
-        Worker worker3 = new Worker();
-        worker3.Name = "Логан";
-        worker3.Surname = "Ямамото";
-        worker3.Rate = 21000;
-        worker3.days = 25;
-        
-        Worker worker4 = new Worker();
-        worker4.Name = "Риана";
-        worker4.Surname = "Беранже";
-        worker4.Rate = 19040;
-        worker4.days = 20;
-        
-        double zp = Worker.GetSalary();
-        Console.WriteLine($"Зарплата работника {Worker.Name} {Worker.Surname}: {zp}");
-    }
+        Numbers numbers = new Numbers(7868546, 7508787);
+        numbers.DisplayNumbers();
+        Console.WriteLine($"Сумма чисел: {numbers.Sum()}");
+        Console.WriteLine($"Наибольшее значение: {numbers.Max()}"); 
+    } 
 }
